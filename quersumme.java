3d2b537053e76a3f331ba9df86d47ebd0ca3aa49
sycrw <114520804+sycrw@users.noreply.github.com>
2023-01-16 14:10:57 +0000

    factorial

--- quersumme.java
+++ quersumme.java
@@ -1,8 +1,10 @@
 public class test{
-  static int one =200;
+  static int one =40;
   public static void main(String[] args){
+    System.out.println(one);
     System.out.println("Quersumme: " +quersumme(one));
     System.out.println("Sum: "+sum(one));
+    System.out.println("Fact: "+fact(one));
   }
   public static long quersumme(long n){
      if(n>0){
@@ -18,6 +20,16 @@ public class test{
     }
     else{
       return n + sum(n-1);
+    }
+  }
+  public static long fact(long n){
+    if(n==1){
+      return 1;
+    }else if(n<=0){
+      return 1;
+    }
+    else{
+      return n * fact(n-1);
     }
   }
 }
